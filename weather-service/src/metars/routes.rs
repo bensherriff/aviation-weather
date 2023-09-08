8c2a6368ad59cc4e42e0e1cb83609a9f676d1828
Ben Sherriff <bsherriffj@gmail.com>
2023-09-07 21:30:46 -0400

    Edit create migrations

--- weather-service/src/metars/routes.rs
+++ weather-service/src/metars/routes.rs
@@ -2,17 +2,6 @@ use crate::error_handler::CustomError;
 use crate::metars::Metars;
 use actix_web::{get, web, HttpResponse, Responder};
 
-// #[get("metars/{ids}")]
-// async fn get_all(ids: web::Path<String>) -> Result<HttpResponse, CustomError> {
-//     let airports = web::block(|| Ok::<_, CustomError>(async {Metars::get_all(ids.into_inner()).await}))
-//         .await
-//         .unwrap()
-//         .unwrap()
-//         .await
-//         .unwrap();
-//     Ok(HttpResponse::Ok().json(airports))
-// }
-
 #[get("metars/{ids}")]
 async fn get_all(ids: web::Path<String>) -> impl Responder {
     let airports = web::block(|| Ok::<_, CustomError>(async {Metars::get_all(ids.into_inner()).await}))
