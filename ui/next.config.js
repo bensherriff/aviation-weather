8438acc68bcd88d8d048214d331d2b7fdc8d1a6d
Ben Sherriff <bsherriffj@gmail.com>
2023-12-19 16:46:11 -0500

    Updated ui dockerfile/package.json

--- ui/next.config.js
+++ ui/next.config.js
@@ -5,13 +5,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
   },
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300
-    };
-    return config;
-  },
+  // webpackDevMiddleware: (config) => {
+  //   config.watchOptions = {
+  //     poll: 1000,
+  //     aggregateTimeout: 300
+  //   };
+  //   return config;
+  // },
   publicRuntimeConfig: {
     // remove private variables from processEnv
     processEnv: Object.fromEntries(Object.entries(process.env).filter(([key]) => key.includes('NEXT_PUBLIC_')))
